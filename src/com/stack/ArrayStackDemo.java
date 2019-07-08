99b5794b808dbb1e82f346bca6299ca48bca1bbb
1771979508 <1771979508@qq.com>
2019-07-08 22:14:50 +0800

    栈的push、pop、list等操作

---
+++ src/com/stack/ArrayStackDemo.java
@@ -0,0 +1,72 @@
+package com.stack;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年7月8日 下午9:56:46	
+*/
+public class ArrayStackDemo {
+	
+	
+	
+}
+
+// 定义一个ArrayStack biao表示栈
+class ArrayStack{
+	private int maxSize;  // 栈的大小
+	private int[] stack;  // 数组，数组模拟栈，数据就放在该数组
+	private int top = -1; // top表示栈顶，初始化为-1
+	
+	// 构造器
+	public ArrayStack(int maxSize){
+		this.maxSize = maxSize;
+		stack = new int[this.maxSize];
+	}
+	
+	// 栈满
+	public boolean isFull(){
+		return top == maxSize - 1;
+	}
+	
+	// 栈空
+	public boolean isEmpty(){
+		return top == -1;
+	}
+	
+	// 入栈
+	public void push(int value){
+		// 先判断栈是否满
+		if(isFull()){
+			System.out.println("栈满");
+			return;
+		}
+		top++;
+		stack[top] = value;
+	}
+	
+	// 出栈
+	public int pop(){
+		// 先判断栈是否为空
+		if(isEmpty()){
+			// 抛出异常
+			throw new RuntimeException("栈空，没有数据~");
+		}
+		// 取栈顶的元素
+		int value = stack[top];
+		top--;
+		return value;
+	}
+	
+	// 遍历栈中的数据 -> 遍历时，需要从栈顶开始显示数据
+	public void list(){
+		if(isEmpty()){
+			System.out.println("栈空，没有数据~");
+			return;
+		}
+		// 需要从栈顶开始显示数据
+		for(int i=top;i>=0;i--){
+			System.out.printf("stack[%d]=%d\n",i,stack[i]);
+		}
+	}
+	
+	
+	
+}
