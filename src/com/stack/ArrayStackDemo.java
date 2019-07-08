3b8e7017bd938d79009804ec1b3e18de9f3af9e9
1771979508 <1771979508@qq.com>
2019-07-08 22:34:29 +0800

    测试栈的相关方法

--- src/com/stack/ArrayStackDemo.java
+++ src/com/stack/ArrayStackDemo.java
@@ -1,11 +1,56 @@
 package com.stack;
+
+import java.util.Scanner;
+
 /**
 *	@author 作者 Joker青
 *	@version 创建时间：2019年7月8日 下午9:56:46	
 */
+
+
 public class ArrayStackDemo {
 	
-	
+	public static void main(String[] args) {
+		
+		ArrayStack stack = new ArrayStack(4);
+		String key = "";
+		boolean loop = true; // 控制是否退出菜单
+		Scanner scanner = new Scanner(System.in);
+		
+		while(loop){
+			System.out.println("show: 表示显示栈");
+			System.out.println("exit: 退出程序");
+			System.out.println("push: 表示添加数据到栈(入栈)");
+			System.out.println("pop: 表示从栈中取出数据(出栈)");
+			System.out.println("请输入你的选择");
+			key = scanner.next();
+			switch(key){
+				case "show":
+					stack.list();
+					break;
+				case "push":
+					System.out.println("请输入一个数");
+					int value = scanner.nextInt();
+					stack.push(value);
+					break;
+				case "pop":
+					try{
+						int res = stack.pop();
+						System.out.printf("出栈的数据是%d\n",res);
+					}catch(Exception e){
+						System.out.println(e.getMessage());
+					}
+					break;
+				case "exit":
+					scanner.close();
+					loop = false;
+					break;
+				default:
+					break;
+			}
+		}
+		
+	}
 	
 }
 
