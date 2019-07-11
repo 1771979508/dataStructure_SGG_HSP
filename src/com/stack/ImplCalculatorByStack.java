a8a6e85880d110f3462ccd29cb9328ca4972f8c9
1771979508 <1771979508@qq.com>
2019-07-11 23:06:08 +0800

    完成单位数表达式的计算 计算的结果不知道为啥不对

--- src/com/stack/ImplCalculatorByStack.java
+++ src/com/stack/ImplCalculatorByStack.java
@@ -7,7 +7,73 @@ public class ImplCalculatorByStack {
 	
 	public static void main(String[] args) {
 		
+		// 定义一个字符串
+		String expression = "3+2*6-2";
+		// 创建两个栈
+		ArrayStackForCalculator numStack = new ArrayStackForCalculator(10);
+		ArrayStackForCalculator operStack = new ArrayStackForCalculator(10);
+		// 定义需要的相关变量
+		int index = 0;  // 用于扫面
+		int num1 = 0;
+		int num2 = 0;
+		int oper = 0;
+		int res = 0;
+		char ch = ' ';  // 将每次扫描得到char保存到ch
+		// 开始while循环的扫描expression
+		while(true){
+			
+			// 依次得到expression 的每一个字符
+			ch = expression.substring(index,index+1).charAt(0);
+			
+			// 判断ch是运算符还是数字，然后做相应的处理
+			if(operStack.isOper(ch)){  // 如果是运算符的话
+				// 判断当前符号栈是否为空
+				if(!operStack.isEmpty()){
+					// 如果符号栈有操作符，就进行比较，如果当前操作符的优先级小于或者等于栈中的操作符，就需要从数栈中pop出两个数
+					// 在从符号栈中pop出一个符号，进行运算，将得到的结果入数栈，然后将当前的操作符入符号栈
+					if(operStack.priority(ch) <= operStack.priority(operStack.peek())){
+						num1 = numStack.pop();
+						num2 = numStack.pop();
+						oper = operStack.pop();
+						res = numStack.cal(num1, num2, oper);
+						// 把运算的结果入数栈
+						numStack.push(res);
+						// 然后将当前的操作符入符号栈
+						operStack.push(ch);
+					}else{
+						// 如果当前的操作符的优先级大于栈中的操作符，就直接入符号栈
+						operStack.push(ch);
+					}
+				}else{
+					// 如果符号栈为空的话，则直接入栈
+					operStack.push(ch);
+				}
+			}else{
+				// 如果是数，则直接入数栈
+				numStack.push(ch - 48);  // 这里由于得到的是字符'1',而字符'1'对应的ascii码表值与知道的值相差48
+			}
+			// 让index+1,并判断是否扫描到expression的最后
+			index++;
+			if(index >= expression.length()){
+				break;
+			}
+		} 
 		
+		// 表达式扫描完毕，就开始处理数据 -> 就顺序的从数栈和符号栈中pop各自的符号和数字，运行计算
+		while(true){
+			// 如果符号栈为空，则计算到最后的结果，数栈中只有一个数字【结果】
+			if(operStack.isEmpty()){
+				break;
+			}
+			num1 = numStack.pop();
+			num2 = numStack.pop();
+			oper = operStack.pop();
+			res = numStack.cal(num1, num2, oper);
+			numStack.push(res); // 入栈
+		}
+		int resL = numStack.pop();
+		// 打印一下表达式和结果
+		System.out.printf("表达式 %s = %d",expression,resL);
 		
 	}
 	
@@ -26,6 +92,11 @@ class ArrayStackForCalculator{
 	public ArrayStackForCalculator(int maxSize){
 		this.maxSize = maxSize;
 		stack = new int[this.maxSize];
+	}
+	
+	// 返回栈顶的值
+	public int peek(){
+		return stack[top];
 	}
 	
 	// 栈满
