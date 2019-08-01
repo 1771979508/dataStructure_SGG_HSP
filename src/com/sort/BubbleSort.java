cb8c8bfe6efc7a08c1be39b7c4ee102407db4b78
1771979508 <1771979508@qq.com>
2019-08-01 22:51:55 +0800

    优化排序数组后 - 即在一次排序中如果没有位置变动则直接跳过本次循环

--- src/com/sort/BubbleSort.java
+++ src/com/sort/BubbleSort.java
@@ -15,63 +15,46 @@ public class BubbleSort {
 	
 	public static void main(String[] args) {
 		
-		int[] arr = {3,9,-1,10,-2};
+		int[] arr = {3,9,-1,10,-2,20,30};
+//		int[] arr = {1,2,3,4};
 		
-		// 为了容易理解，我们把冒泡排序的演变过程展示出来
+		System.out.println("排序前的数组为："+Arrays.toString(arr));
 		
+		bubble(arr);
+		
+		System.out.println("排序后的数组为："+Arrays.toString(arr));
+		
+	}
+	
+	public static void bubble(int[] arr){
 		// 第一堂趟排序，就是将最大的数排在最后
 		int temp = 0;  // 临时变量
+		
+		boolean flag = false;  // 标识变量，表示是否j进行过交换
+		
 		for(int i=0;i<arr.length-1;i++){
 			for(int j=0;j<arr.length-1-i;j++){
 				// 如果前面的数比后面的数大，则交换
 				if(arr[j] > arr[j+1]){
+					
+					// 如果进行交换则将flag置为true
+					flag = true;
+					
 					temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
 				}
 			}
-			System.out.println("第"+(i+1)+"趟排序后的数组为：");
-			System.out.println(Arrays.toString(arr));
-		}
-		/*
-		// 第二趟排序，就是将第二大的数排在倒数第二位
-		for(int j=0;j<arr.length-1-1;j++){
-			// 如果前面的数比后面的数大，则交换
-			if(arr[j] > arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
+//			System.out.println("第"+(i+1)+"趟排序后的数组为：");
+//			System.out.println(Arrays.toString(arr));
+			
+			if(!flag){  //在一趟排序中，一次都没有交换过，则直接跳过本次的内循环
+				break;
+			}else {
+				flag = false;  // 重置flag!!!,进行下一次判断
 			}
+			
 		}
-		
-		System.out.println("第二趟排序后的数组为：");
-		System.out.println(Arrays.toString(arr));
-		
-		for(int j=0;j<arr.length-1-1-1;j++){
-			// 如果前面的数比后面的数大，则交换
-			if(arr[j] > arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}
-		}
-		
-		System.out.println("第三趟排序后的数组为：");
-		System.out.println(Arrays.toString(arr));
-		
-		for(int j=0;j<arr.length-1-1-1-1;j++){
-			// 如果前面的数比后面的数大，则交换
-			if(arr[j] > arr[j+1]){
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}
-		}
-		
-		System.out.println("第四趟排序后的数组为：");
-		System.out.println(Arrays.toString(arr));
-		*/
-		
 	}
 	
 }
