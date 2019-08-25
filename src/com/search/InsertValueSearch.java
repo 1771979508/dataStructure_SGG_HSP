9905dffd15017c2ad4062de94f093ad93c4f043d
1771979508 <1771979508@qq.com>
2019-08-25 23:12:00 +0800

    插值查找代码实现 - 思路是基于二分查找的基础上进行

---
+++ src/com/search/InsertValueSearch.java
@@ -0,0 +1,15 @@
+package com.search;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年8月25日 下午11:07:33	
+*/
+public class InsertValueSearch {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+	
