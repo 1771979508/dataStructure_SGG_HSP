9bebd5e7acaecbf9c893f1618358fbda4117a206
1771979508 <1771979508@qq.com>
2019-08-28 22:41:07 +0800

    哈希表完成控制台功能和查找的代码

--- src/com/hashTable/HashTableDemo.java
+++ src/com/hashTable/HashTableDemo.java
@@ -1,4 +1,7 @@
 package com.hashTable;
+
+import java.util.Scanner;
+
 /**
 *	@author 作者 Joker青
 *	@version 创建时间：2019年8月27日 下午10:46:40	
@@ -7,7 +10,45 @@ public class HashTableDemo {
 	
 	public static void main(String[] args) {
 		
+		// 实例化哈希表
+		HashTable hashTable = new HashTable(7);
 		
+		// 写一个简单的菜但
+		String key = "";
+		Scanner scanner = new Scanner(System.in);
+		while(true){
+			System.out.println("add: 添加雇员");
+			System.out.println("find: 查找雇员");
+			System.out.println("list: 显示雇员");
+			System.out.println("exit: 退出系统");
+			
+			key = scanner.next();
+			switch (key) {
+			case "add":
+				System.out.println("输入id");
+				int id = scanner.nextInt();
+				System.out.println("输入名字");
+				String name = scanner.next();
+				// 创建雇员
+				Emp emp = new Emp(id,name);
+				hashTable.add(emp);
+				break;
+			case "find":
+				System.out.println("请输入要查找的id");
+				id = scanner.nextInt();
+				hashTable.findEmpById(id);
+				break;
+			case "list":
+				hashTable.list();
+				break;
+			case "exit":
+				scanner.close();
+				System.exit(0);
+			default:
+				break;
+			}
+			
+		}
 		
 	}
 	
@@ -26,7 +67,11 @@ class HashTable{
 		empLinkedListArray = new EmpLinkedList[size];
 		
 		// 留下了一个坑？
-		
+		// 这里如果不写的话，添加雇员的方法时 会抛异常,因为在创建的数组中，初始化为null，将员工类Emp链在其后面显然不合理
+		// 需要初始化链表
+		for(int i=0;i<size;i++){
+			empLinkedListArray[i] = new EmpLinkedList();  // 初始化的时候将其引用为链表类型
+		}
 	}
 	
 	// 添加雇员
@@ -40,9 +85,22 @@ class HashTable{
 	// 遍历所有的链表，遍历hashTable
 	public void list(){
 		for(int i=0;i<size;i++){
-			empLinkedListArray[i].list();
+			empLinkedListArray[i].list(i);
 		}
 	}
+	
+	//虽然在链表中写的查找雇员的方法，但是调用还是在散列表中
+	public void findEmpById(int id){
+		// 使用散列函数确定到哪条链表中查找
+		int empLinkedListNO = hashFun(id);
+		Emp emp = empLinkedListArray[empLinkedListNO].findEmpById(id);
+		if(emp != null){
+			System.out.printf("在第%d条链表中找到雇员id = %d\n",empLinkedListNO,id);
+		}else{
+			System.out.println("在哈希表中，没有找到雇员");
+		}
+	}
+	
 	
 	// 编写散列函数，使用取模法
 	public int hashFun(int id){
@@ -115,12 +173,12 @@ class EmpLinkedList{
 	}
 	
 	// 遍历链表的雇员信息
-	public void list(){
+	public void list(int no){
 		if(head == null){ // 说明链表为空
-			System.out.println("链表为空");
+			System.out.println("第"+no+"条链表为空");
 			return;
 		}
-		System.out.println("当前链表的信息为");
+		System.out.print("第"+no+"链表的信息为");
 		Emp currentEmp = head;  // 辅助指针
 		while(true){
 			System.out.printf("=> id=%d name=%s\t",currentEmp.id,currentEmp.name);
@@ -130,6 +188,33 @@ class EmpLinkedList{
 			currentEmp = currentEmp.next;  // 后移，遍历
 		}
 		System.out.println();
+	}
+	
+	
+	// 根据id查找雇员
+	// 如果查找到，就返回Emp，如果没有找到，就返回null
+	public Emp findEmpById(int id){
+		// 判断链表是否为空
+		if(head == null){
+			System.out.println("链表为空");
+			return null;
+		}
+		// 辅助指针
+		Emp curEmp = head;
+		while(true){
+			if(curEmp.id == id){  // 找到
+				break;  // 这时curEmp就指向要查找的雇员
+			}
+			// 退出
+			if(curEmp.next == null){  // 说明遍历当前链表没有找到该雇员
+				curEmp = null;
+				break;
+			}
+			curEmp = curEmp.next;  //  向后移动
+		}
+		
+		return curEmp;
+		
 	}
 	
 	
