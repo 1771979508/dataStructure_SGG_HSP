467cabf7af7da6411f1419868faec468288ecaf7
1771979508 <1771979508@qq.com>
2019-09-19 23:32:05 +0800

    二叉树线索化测试

--- src/com/tree/threadbinarytree/ThreadBinaryTreeDemo.java
+++ src/com/tree/threadbinarytree/ThreadBinaryTreeDemo.java
@@ -7,13 +7,44 @@ package com.tree.threadbinarytree;
 */
 public class ThreadBinaryTreeDemo {
 	
-	
+	public static void main(String[] args) {
+		
+		// 测试中序线索二叉树的功能
+		HeroNode root = new HeroNode(1,"tom");
+		HeroNode node2 = new HeroNode(3,"jack");
+		HeroNode node3 = new HeroNode(6,"smith");
+		HeroNode node4 = new HeroNode(8,"mary");
+		HeroNode node5 = new HeroNode(10,"king");
+		HeroNode node6 = new HeroNode(14,"dim");
+		
+		// 二叉树，后面我们要递归创建，现在简单手动创建
+		root.setLeft(node2);
+		root.setRight(node3);
+		node2.setLeft(node4);
+		node2.setRight(node5);
+		node3.setLeft(node6);
+		
+		// 测试中序线索化
+		ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
+		threadedBinaryTree.setRoot(root);
+		// 调用重载后的方法
+		threadedBinaryTree.threadedNodes();
+		
+		// 测试：以10号节点测试
+		HeroNode leftNode = node5.getLeft();
+		System.out.println("10号节点的前驱节点为："+leftNode);
+		HeroNode rightNode = node5.getRight();
+		System.out.println("10号节点的后继节点为："+rightNode);
+		
+		
+		
+	}
 	
 }
 
 
 //ThreadedBinaryTree 实现了线索化功能的二叉树
-class BinaryTree{
+class ThreadedBinaryTree{
 	private HeroNode root;
 	
 	// 为了实现线索化，需要创建给指向节点当前节点的前驱节点的指针
@@ -24,6 +55,10 @@ class BinaryTree{
 		this.root = root;
 	}
 	
+	// 重载一下threadedNodes方法
+	public void threadedNodes(){
+		this.threadedNodes(root);
+	}
 	
 	// 编写对二叉树进行中序遍历线索化的方法
 	/**
