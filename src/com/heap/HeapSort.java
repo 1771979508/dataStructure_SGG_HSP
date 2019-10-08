be01f708f5f6e8c512cfc0788361bbb3461f122b
1771979508 <1771979508@qq.com>
2019-10-08 23:34:02 +0800

    完成最大二叉堆的函数编写

--- src/com/heap/HeapSort.java
+++ src/com/heap/HeapSort.java
@@ -28,7 +28,23 @@ public class HeapSort {
 	 * @param length	表示对多少个元素进行调整， length 是逐步减少的
 	 */
 	public static void adjustHeap(int arr[],int i,int length){
-		
+		int temp = arr[i]; // 先取出当前元素的值，保存在临时变量中
+		// 开始调整
+		// 说明
+		// 1.k=i*2+1 其中，k为i节点的左子节点
+		for(int k=i*2+1;k<length;k=k*2+1){   // k=k*2+1 为啥？
+			if(k+1<length && arr[k] < arr[k+1]){
+				k++;  // k 指向右子节点
+			}
+			if(arr[k] > temp){  // 如果子节点大于父节点
+				arr[i] = arr[k];  // 把较大的值赋给当前节点
+				i = k;  // ！！！important 将i指向k，继续循环比较
+			}else{
+				break;  // 由于树是从左向右，从上往下依次生成的
+			}
+		}
+		// 当for 循环结束后，我们已经将以i为父节点的树的最大值，放在了最顶部(局部)
+		arr[i] = temp;  // 将temp值放到调整后的位置
 	}
 	
 }
