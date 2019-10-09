81e63d2823aa4ab5b355dff072f047c9369aa751
1771979508 <1771979508@qq.com>
2019-10-09 22:14:36 +0800

    完成堆排序代码的编写

--- src/com/heap/HeapSort.java
+++ src/com/heap/HeapSort.java
@@ -1,4 +1,7 @@
 package com.heap;
+
+import java.util.Arrays;
+
 /**
 *	@author 作者 Joker青
 *	@version 创建时间：2019年10月8日 下午10:43:11	
@@ -9,13 +12,45 @@ public class HeapSort {
 		
 		// 要求将数组进行升序排序
 		int arr[] = {4,6,8,5,9};
-		
+		heapSort(arr);
 		
 	}
 	
 	// 编写一个堆排序的方法
 	public static void heapSort(int arr[]){
+		int temp = 0;
 		System.out.println("堆排序！！");
+		
+		// 先进行分步实现
+//		adjustHeap(arr,1,arr.length);
+//		System.out.println("第一次排序完的结果" + Arrays.toString(arr));
+//		adjustHeap(arr,0,arr.length);
+//		System.out.println("第二次排序完的结果" + Arrays.toString(arr));
+		
+		
+		// 完成最终版代码
+		// 将无序序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆
+		for(int i=arr.length/2-1;i>=0;i--){
+			adjustHeap(arr, i, arr.length);   // 得到的是已经将最大的元素放置到堆顶，即根节点
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		// 2.将堆顶元素与末尾元素交换，将最大元素 “沉” 到数组末端
+		// 3.重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素，【反复执行调整和交换】步骤，直到整个序列有序
+		for(int j=arr.length-1;j>0;j--){  // 这里只需要对数组-1个数进行交换即可
+			// 把末尾元素存到临时变量中
+			temp = arr[j];
+			// 把最大元素(根节点)交换到末尾元素
+			arr[j] = arr[0];
+			// 把temp元素还给第一个元素
+			arr[0] = temp;
+			
+			// 交换完成之后，即剔除了数组末尾的最大元素，继续调整大顶堆、交换
+			adjustHeap(arr, 0, j);  // 从上往下走，所以从0开始
+			
+		}
+		System.out.println(Arrays.toString(arr));
+		
 	}
 	
 	// 将一个数组（完全二叉树），调整成一个大顶堆
