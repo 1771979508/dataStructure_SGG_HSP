1943f73bec222f978bc0ba101c2f981382315b2d
1771979508 <1771979508@qq.com>
2019-10-09 23:26:50 +0800

    哈夫曼树

---
+++ src/com/huffmantree/HuffmanTree.java
@@ -0,0 +1,18 @@
+package com.huffmantree;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年10月9日 下午11:26:02	
+*/
+
+
+/* 哈夫曼树 */
+public class HuffmanTree {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+	
