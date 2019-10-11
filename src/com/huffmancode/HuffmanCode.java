538758fb25d7951ae32832506e8fe663168f732d
1771979508 <1771979508@qq.com>
2019-10-11 23:07:03 +0800

    赫夫曼编码类的创建及思路图

---
+++ src/com/huffmancode/HuffmanCode.java
@@ -0,0 +1,18 @@
+package com.huffmancode;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年10月11日 下午11:06:01	
+*/
+
+/*赫夫曼编码*/
+
+public class HuffmanCode {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+	
