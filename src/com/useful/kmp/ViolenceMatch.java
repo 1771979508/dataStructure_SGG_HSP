40674a4c19c0a1d1ae799d114ff538ab5cc7b9a5
1771979508 <1771979508@qq.com>
2019-10-30 23:29:45 +0800

    字符串的暴力匹配算法代码实现

---
+++ src/com/useful/kmp/ViolenceMatch.java
@@ -0,0 +1,58 @@
+package com.useful.kmp;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年10月30日 下午11:17:34	
+*/
+
+/*
+ * 
+ * 		暴力匹配算法	
+ * 
+ * 
+ * */
+public class ViolenceMatch {
+	
+	public static void main(String[] args) {
+		
+		// 测试暴力匹配算法
+		String str1 = "硅硅谷 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
+		String str2 = "尚硅谷你尚硅你";
+		int index = violenceMatch(str1, str2);
+		System.out.println("index="+index);
+		
+	}
+	
+	// 暴力匹配算法实现
+	public static int violenceMatch(String str1,String str2){
+		char[] c1 = str1.toCharArray();
+		char[] c2 = str2.toCharArray();
+		
+		int c1Len = c1.length;
+		int c2Len = c2.length;
+		
+		int i=0;  // 索引指向 c1
+		int j=0;  // 索引指向c2
+		
+		while(i<c1Len && j<c2Len){
+			if(c1[i] == c2[j]){
+				i++;
+				j++;
+			}else{
+				i = i-(j-1);
+				j=0;
+			}
+		}
+		
+		// 判断是否成功
+		if(j == c2Len){
+			return i-j;
+		}else{
+			return -1;
+		}
+		
+	}
+	
+	
+	
+}
+	
