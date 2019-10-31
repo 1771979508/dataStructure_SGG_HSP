da1f0d760fa3380a94e8592d9b6846d0802f117d
1771979508 <1771979508@qq.com>
2019-10-31 22:49:43 +0800

    kmp字符串匹配算法 以及暴力匹配算法

---
+++ src/com/useful/kmp/kmpNext.java
@@ -0,0 +1,23 @@
+package com.useful.kmp;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年10月31日 下午10:45:27	
+*/
+public class kmpNext {
+	
+	public static void main(String[] args) {
+		
+		String str1 = "BBC ABCDAB ABCDABCDABDE";
+		String str2 = "ABCDABD";
+		
+	}
+	
+	// 获取到一个字符串(字串)的部分匹配值表
+	public static int[] kmpNext(String dest){
+		// 创建一个next数组保存部门匹配值
+		int[] next = new int[dest.length()];
+		
+	}
+	
+}
+	
