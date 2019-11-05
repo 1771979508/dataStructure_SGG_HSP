fb372d9b65647408674e7d74d132edf48e1dd26f
1771979508 <1771979508@qq.com>
2019-11-05 23:33:06 +0800

    测试排序 及相关方法的代码实现

--- src/com/useful/kruskal/KruskalCase.java
+++ src/com/useful/kruskal/KruskalCase.java
@@ -1,4 +1,7 @@
 package com.useful.kruskal;
+
+import java.util.Arrays;
+
 /**
 *	@author 作者 Joker青
 *	@version 创建时间：2019年11月5日 下午10:01:19	
@@ -8,6 +11,9 @@ package com.useful.kruskal;
 /*
  * 
  * 		克鲁斯卡尔算法 - 解决公交站问题
+ * 
+ * 			1.对图的所有边按照权值大小进行排序
+ * 			2.将边添加到最小生成树中时
  * 
  * 
  * */
@@ -40,6 +46,15 @@ public class KruskalCase {
 		KruskalCase kruskalCase = new KruskalCase(vertexs, matrix);
 		kruskalCase.print();
 		
+		System.out.println("未对顶点排序前："+Arrays.toString(kruskalCase.getEdges()));
+		
+		/*测试排序代码*/
+		EData[] edges = kruskalCase.getEdges();
+		System.out.println("排序前="+Arrays.toString(edges));		// 没有排序
+		kruskalCase.sortEdges(edges);
+		System.out.println("排序后="+Arrays.toString(edges));
+		
+		
 	}
 	
 	// 构造器
@@ -63,7 +78,7 @@ public class KruskalCase {
 		
 		// 统计边
 		for(int i=0;i<vlen;i++){
-			for(int j=0;j<vlen;j++){
+			for(int j=i+1;j<vlen;j++){
 				if(this.matrix[i][j] != INF){
 					edgeNum++;
 				}
@@ -84,5 +99,80 @@ public class KruskalCase {
 		}
 	}
 	
+	/**
+	 * 功能：对边进行排序处理，冒泡排序
+	 * @param edges	边的集合
+	 */
+	private void sortEdges(EData[] edges){
+		for(int i=0;i<edges.length-1;i++){
+			for(int j=0;j<edges.length-1-i;j++){
+				if(edges[j].weight > edges[j+1].weight){  // 交换
+					EData tmp = edges[j];
+					edges[j] = edges[j+1];
+					edges[j+1] = tmp;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param ch	ch顶点的值，比如'A','B'
+	 * @return		返回ch顶点对应的下标，如果找不到，就返回-1
+	 */
+	private int getPosition(char ch){
+		for(int i=0;i<vertexs.length;i++){
+			if(vertexs[i] == ch){	// 找到
+				return i;
+			}
+		}
+		// 找不到返回-1
+		return -1;
+	}
+	
+	/**
+	 * 
+	 * @return		构造边  类似  [EData [start=E, end=F, weight=2]....]
+	 */
+	private EData[] getEdges(){
+		int index = 0;
+		EData[] edges = new EData[edgeNum];
+		for(int i=0;i<vertexs.length;i++){
+			for(int j=i+1;j<vertexs.length;j++){  // 自己不和自己判断
+//				for(int j=0;j<vertexs.length;j++){
+				if(matrix[i][j] != INF){
+					edges[index++] = new EData(vertexs[i], vertexs[j], matrix[i][j]);
+				}
+			}
+		}
+		return edges;
+	}
+	
+	
+	
 }
+
+// 创建一个类EData，它的对象实例表示一条边
+class EData{
+	char start;  // 边的一个点
+	char end;	// 边的另外一个点
+	int weight;	// 边的权值
+	// 构造器
+	public EData(char start,char end,int weight){
+		this.start = start;
+		this.end = end;
+		this.weight = weight;
+	}
+	// 重写toString，便于输出边信息
+	@Override
+	public String toString() {
+		return "EData [start=" + start + ", end=" + end + ", weight=" + weight + "]";
+	}
+	
+}
+
+
+
+
+
 	
