adc63c5edd31a60df5e59836c91a90af2e9b623b
1771979508 <1771979508@qq.com>
2019-11-07 22:39:04 +0800

    迪杰斯特拉算法 - 解决最短路径问题

---
+++ src/com/useful/dijkstra/DijkstraAlgorithm.java
@@ -0,0 +1,22 @@
+package com.useful.dijkstra;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年11月7日 下午10:37:56	
+*/
+
+/*
+ * 
+ * 		迪杰斯特拉算法 - 解决最短路径问题		
+ * 
+ * 
+ */
+public class DijkstraAlgorithm {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+	
