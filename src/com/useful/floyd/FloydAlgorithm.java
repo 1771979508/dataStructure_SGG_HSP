09251769017c06dbf69abee6e6eda0cd4dd905fc
1771979508 <1771979508@qq.com>
2019-11-09 14:46:14 +0800

    弗洛伊德算法

---
+++ src/com/useful/floyd/FloydAlgorithm.java
@@ -0,0 +1,31 @@
+package com.useful.floyd;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年11月9日 下午2:44:19	
+*/
+
+/**
+ * 
+ * @author ABC 	
+ * 
+ * 弗洛伊德算法：
+ * 		每一个顶点都是出发访问点，所以需要将每一个顶点看作被访问顶点，求出从每一个顶点到其它顶点的最短路径
+ *
+ */
+
+
+public class FloydAlgorithm {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+
+// 创建图
+class Graph{
+	
+}
+	
