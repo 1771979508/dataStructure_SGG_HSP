cb6e4f2e26dc3c349f8f696014a2573cb70d4f5c
1771979508 <1771979508@qq.com>
2019-11-09 14:45:49 +0800

    弗洛伊德算法与迪杰斯特拉算法，两种算法的区别

--- src/com/useful/dijkstra/DijkstraAlgorithm.java
+++ src/com/useful/dijkstra/DijkstraAlgorithm.java
@@ -13,6 +13,17 @@ import java.util.Arrays;
  * 
  * 
  */
+
+
+/*
+ * 	弗洛伊德算法：
+ * 		每一个顶点都是出发访问点，所以需要将每一个顶点看作被访问顶点，求出从每一个顶点到其它顶点的最短路径
+ * 
+ *  迪杰斯特拉算法：
+ * 		通过选定的被访问顶点，求出从出发访问顶点到其它顶点的最短路径
+ * 
+ * */
+
 public class DijkstraAlgorithm {
 	
 	public static void main(String[] args) {
