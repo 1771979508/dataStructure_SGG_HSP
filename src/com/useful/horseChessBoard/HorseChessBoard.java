0ba2758272bdb103fbef5846dfc1ca7464fb43c8
1771979508 <1771979508@qq.com>
2019-11-14 22:32:45 +0800

    用贪心算法优化马踏棋盘以减少回溯的次数

--- src/com/useful/horseChessBoard/HorseChessBoard.java
+++ src/com/useful/horseChessBoard/HorseChessBoard.java
@@ -6,6 +6,7 @@ package com.useful.horseChessBoard;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * 
@@ -79,6 +80,10 @@ public class HorseChessBoard {
 		visited[row*X+column] = true; // 标记该位置已经访问
 		// 获取当前位置可以走的下一个位置的集合
 		ArrayList<Point> ps = next(new Point(column,row));
+		
+		// 对ps进行排序，排序的规则就是对ps的所有的Point对象的下一步的位置的数目，进行非递减排序
+		sort(ps);	
+		
 		// 遍历ps
 		while(!ps.isEmpty()){
 			Point p = ps.remove(0);  // 取出下一个可以走的位置
@@ -144,6 +149,30 @@ public class HorseChessBoard {
 		}
 		return ps;
 	}
+	
+	// 根据当前这个一步的所有下一步的选择位置，进行非递减排序，减少回溯的次数
+	public static void sort(ArrayList<Point> ps){
+		ps.sort(new Comparator<Point>() {
+
+			@Override
+			public int compare(Point o1, Point o2) {
+				// 获取到o1的下一步的所有位置个数
+				int count1 = next(o1).size();
+				// 获取到o2的下一步的所有位置的个数
+				int count2 = next(o2).size();
+				if(count1 < count2){
+					return -1;
+				}else if(count1 == count2){
+					return 0;
+				}else{
+					return 1;
+				}
+			}
+			
+		});
+	}
+	
+	
 	
 }
 	
